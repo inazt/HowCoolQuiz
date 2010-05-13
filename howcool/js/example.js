271c25a6de357a2fe4a0198d1e11d1ec74bad72b
Sompop <opendream@opendream-sl410-1.(none)>
2010-05-13 16:14:47 +0700

    update result

--- howcool/js/example.js
+++ howcool/js/example.js
@@ -7,5 +7,7 @@ $(document).ready(function() {
    $('#carbon-year').score(Drupal.settings.howcool.carbonyear,"carbon");
    
    $('#weight').html(Drupal.settings.howcool.weight+" ก.ก.");
-      $('#weight2').html(Drupal.settings.howcool.weight+" ก.ก.")
+   $('#weight2').html(Drupal.settings.howcool.weight+" ก.ก.");
+   $('#point_water').html(Drupal.settings.howcool.point_water+" ลิตร");
+   $('$point_carbon').html(Drupal.settings.howcool.point_carbon+" กิโลกรรม");
 });
