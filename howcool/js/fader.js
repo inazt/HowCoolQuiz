71346e5a39c4ea2dd3fab065cc3fc7875cc699aa
Nat Weerawan <nat.wrw@gmail.com>
2010-05-26 18:08:01 +0700

    Change check page style

--- howcool/js/fader.js
+++ howcool/js/fader.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.cool_check .form-checkboxes label').mouseover(function () {
+  $('.cool_check .form-checkboxes img').mouseover(function () {
   $(this).css({cursor:'pointer'});
   $(this).fadeOut('fast').fadeIn('slow');
   });
