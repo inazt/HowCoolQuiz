a2561abb827d90217643a9c6f3857be1e750639a
Nat Weerawan <nat.wrw@gmail.com>
2010-05-28 01:17:31 +0700

    Check browser

--- howcool/js/customSelect.js
+++ howcool/js/customSelect.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 	var marker = $('form .error').removeClass('error')
 	$('.form-select').customStyle().css({left:$(".pop label").css("width")});
 	marker.siblings().addClass('customError');
+	$('.pop > .form-item > select').width($('.pop > .form-item > span').get(0).offsetWidth);
+	$('.pop > .form-item > select').height($('.pop > .form-item > span').get(0).offsetHeight);
+ //	$('.pop > .form-item > select').css({'font-size':'20px', 'line-height':'20px'});
 //	$(".pop .form-select").css({left:$(".pop label").css("width")});
 });
 
