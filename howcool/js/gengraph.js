e75b3a5f99c9c3e3e4234d85108e17d81cc5e14f
Sompop Kulapalanont <sompop.kulapalanont@gmail.com>
2010-05-27 12:44:29 +0700

    update data

--- howcool/js/gengraph.js
+++ howcool/js/gengraph.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
    $('#weightz').html(Drupal.settings.howcool.weight+" ก.ก.");
    $('#point_water').html(Drupal.settings.howcool.point_water+" ลิตร");
    $('#point_carbon').html(Drupal.settings.howcool.point_carbon+" กิโลกรรม");
-   $('#point_water_year').html(Drupal.settings.howcool.point_water*365+" ลิตร");
-   $('#point_carbon_year').html(Drupal.settings.howcool.point_carbon*365+" กิโลกรรม");
+   $('#point_water_year').html(Math.ceil(Drupal.settings.howcool.point_water*365)+" ลิตร");
+   $('#point_carbon_year').html(Math.ceil(Drupal.settings.howcool.point_carbon*365)+" กิโลกรรม");
    $('#show_image').html(Drupal.settings.howcool.image+" กิโลกรรม");
    
 });
