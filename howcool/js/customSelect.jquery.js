3ab6a0235cdfe24634830349815f573c88512aa5
Sompop Kulapalanont <sompop.kulapalanont@gmail.com>
2010-05-28 11:11:45 +0700

    do nothing from yesterday

--- howcool/js/customSelect.jquery.js
+++ howcool/js/customSelect.jquery.js
@@ -6,14 +6,14 @@
 	  return this.each(function() {
 	  
 			var currentSelected = $(this).find(':selected');
-			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+currentSelected.text()+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
+			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+currentSelected.text()+'</span></span>').css({'top':0,position:'absolute', opacity:0,fontSize:$(this).next().css('font-size') });
 			var selectBoxSpan = $(this).next();
-			//var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left')) -parseInt(selectBoxSpan.css('padding-right'));	
-			var selectBoxWidth = parseInt($(this).width());			
+			var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left')) -parseInt(selectBoxSpan.css('padding-right'));	
+			//var selectBoxWidth = parseInt($(this).width());			
 			var selectBoxSpanInner = selectBoxSpan.find(':first-child');
 			selectBoxSpan.css({display:'inline-block'});
 			selectBoxSpanInner.css({width:selectBoxWidth, display:'inline-block'});
-			var selectBoxHeight = parseInt(selectBoxSpan.height()) + parseInt(selectBoxSpan.css('padding-top')) + parseInt(selectBoxSpan.css('padding-bottom'));
+			var selectBoxHeight = 25;
 			$(this).height(selectBoxHeight).change(function(){
 				selectBoxSpanInner.text($(this).find(':selected').text()).parent().removeClass('customError').addClass('changed');
 			});
